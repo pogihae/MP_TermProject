ea30038caa891efc1edd51bb9e8fb275ef037cab
Yunho Kim <31121701+Kandreaop@users.noreply.github.com>
2022-04-10 00:27:18 +0900

    초기설정창_1

---
+++ app/src/main/java/com/example/wiuh/InitialSetupActivity.java
@@ -0,0 +1,14 @@
+package com.example.wiuh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InitialSetupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial_setup);
+    }
+}
