302d1b82fc7d5f5da4a422a5169ab10ad093f454
Kim Y.C <yckim19@naver.com>
2022-04-11 00:38:22 +0900

    activity전환 추가

--- app/src/main/java/com/example/wiuh/SigninActivity.java
+++ app/src/main/java/com/example/wiuh/SigninActivity.java
@@ -1,6 +1,8 @@
 package com.example.wiuh;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,14 @@ public class SigninActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+    }
+    public void clickSignin_confirm(View view) {
+            //MainActivity class 실행..
+
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        //SigninActivity 종료 (메모리에서 제거)
+        finish();
+
     }
 }
