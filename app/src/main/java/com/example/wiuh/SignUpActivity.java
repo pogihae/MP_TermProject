c957893a4abf5d7f74032912e0278654f4638138
jomo34 <whdusrua@naver.com>
2022-04-30 16:02:39 +0900

    비밀번호 체크

--- app/src/main/java/com/example/wiuh/SignUpActivity.java
+++ app/src/main/java/com/example/wiuh/SignUpActivity.java
@@ -1,18 +1,28 @@
 package com.example.wiuh;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.wiuh.model.User;
 import com.example.wiuh.util.FirebaseUtil;
 import com.example.wiuh.util.ToastUtil;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * todo: submit 시 모든 칸 채워져 있는지, 중복 검사, 비밀번호 동일
@@ -27,6 +37,7 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText mEtPwd;
     private EditText mEtSecondPwd;
     private TextView idCheck;
+    private ImageView setImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,9 +51,39 @@ public class SignUpActivity extends AppCompatActivity {
         mEtPwd          = findViewById(R.id.signin_password);
         mEtSecondPwd    = findViewById(R.id.signin_check_password);
         idCheck         = findViewById(R.id.signin_id_check);
+        setImage        = (ImageView)findViewById(R.id.setImage);
 
         findViewById(R.id.sighin_submit).setOnClickListener(v -> register());
+
+        idCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ToastUtil.showText(getApplicationContext(), "아이디 중복 버튼 눌림");
+            }
+        });
+
+        mEtSecondPwd.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (mEtPwd.getText().toString().equals(mEtSecondPwd.getText().toString())) {
+                    setImage.setImageResource(R.drawable.ic_true);
+                } else {
+                    setImage.setImageResource(R.drawable.ic_false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
+
 
     //todo
     private boolean isCompleteForm() {
