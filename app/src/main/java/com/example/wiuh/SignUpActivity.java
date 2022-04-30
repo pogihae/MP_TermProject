a243b4cd0231ff9204a40d57cb59185dd5d25e4d
Kim Y.C <yckim19@naver.com>
2022-04-30 17:04:38 +0900

    Update activity_signup.xml
    비밀번호 확인 이미지 위치 변경
    layout 구성 변경

--- app/src/main/java/com/example/wiuh/SignUpActivity.java
+++ app/src/main/java/com/example/wiuh/SignUpActivity.java
@@ -1,6 +1,7 @@
 package com.example.wiuh;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -43,7 +44,8 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         mFirebaseAuth   = FirebaseAuth.getInstance();
         mUserRef        = FirebaseUtil.getUserRef();
 
