a7d7d3a58a9136b740c695a330f6566b0f872312
jomo34 <whdusrua@naver.com>
2022-05-07 00:04:42 +0900

    수정 구현중(버튼누르면 불러오기&취소)

--- app/src/main/java/com/example/wiuh/nav/memo/MemoModify.java
+++ app/src/main/java/com/example/wiuh/nav/memo/MemoModify.java
@@ -2,6 +2,8 @@ package com.example.wiuh.nav.memo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,6 +32,15 @@ public class MemoModify extends AppCompatActivity {
         bulletinTitle.setText(title);
         bulletinBody.setText(body);
 
+        Button cancel = findViewById(R.id.cancel_mod_button);
 
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
+
+
 }
