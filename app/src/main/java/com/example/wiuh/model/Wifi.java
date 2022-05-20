9db27470b7c2f91510db71783fafb2b4b48a2a72
민지원 <mjiwon9971@naver.com>
2022-05-20 23:07:35 +0900

    와이파이 중복 안되게 등록

--- app/src/main/java/com/example/wiuh/model/Wifi.java
+++ app/src/main/java/com/example/wiuh/model/Wifi.java
@@ -7,12 +7,14 @@ import com.google.firebase.database.IgnoreExtraProperties;
 
 @IgnoreExtraProperties
 public class Wifi {
+    public String uid;
     public String SSID;
     public String MAC;
 
     public Wifi() { }
 
-    public Wifi(String SSID, String MAC) {
+    public Wifi(String uid, String SSID, String MAC) {
+        this.uid = uid;
         this.SSID = SSID;
         this.MAC = MAC;
     }
